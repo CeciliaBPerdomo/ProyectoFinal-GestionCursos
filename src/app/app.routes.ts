7101df8060fb43a78a5dc5278f548ceb55fc46b8
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2025-07-17 19:25:02 -0300

    Listado de alumnos en alumno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 import { ListaAlumnos } from './pages/alumnos/lista-alumnos/lista-alumnos';
 
 export const routes: Routes = [
-  { path: 'alumnos', component: ListaAlumnos }
+  { path: 'alumnos', component: ListaAlumnos, data: { modo: 'alumno' } },
+  { path: 'alumnos-admin', component: ListaAlumnos, data: { modo: 'admin' } },
 ];
--- src/app/pages/alumnos/lista-alumnos/lista-alumnos.ts
+++ src/app/pages/alumnos/lista-alumnos/lista-alumnos.ts
@@ -1,5 +1,6 @@
 // app/pages/alumnos/lista-alumnos/lista-alumnos.ts
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 //Material UI 
 import { CommonModule } from '@angular/common';
@@ -16,28 +17,38 @@ import { InscripcionService } from '../../../services/inscripcion.service';
 
 @Component({
   selector: 'app-lista-alumnos',
-    standalone: true,
+  standalone: true,
   imports: [
     CommonModule,
-    MatTableModule, 
+    MatTableModule,
     MatIconModule,
     MatButtonModule
   ],
   templateUrl: './lista-alumnos.html',
-  styleUrls:  ['./lista-alumnos.css']
+  styleUrls: ['./lista-alumnos.css']
 })
 
 export class ListaAlumnos implements OnInit {
+  @Input() modo: 'admin' | 'alumno' = 'admin';
+
   alumnos: Alumno[] = [];
-  displayedColumns: string[] = ['id', 'nombre', 'email', 'cursoNombre', 'acciones'];
+  displayedColumns: string[] = ['id', 'nombre', 'email', 'cursoNombre'];
 
   constructor(
     private alumnoService: AlumnoService,
     private cursoService: CursoService,
-        private inscripcionService: InscripcionService
-  ) {}
+    private inscripcionService: InscripcionService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const modoRuta = this.route.snapshot.data['modo'];
+    this.modo = modoRuta === 'alumno' ? 'alumno' : 'admin';
+
+    if (this.modo === 'admin') {
+      this.displayedColumns.push('acciones');
+    }
+
     this.alumnos = this.alumnoService.getAlumnos();
   }
 
@@ -46,11 +57,10 @@ export class ListaAlumnos implements OnInit {
     return curso ? curso.nombre : 'Desconocido';
   }
 
-   getEstadoInscripcion(alumnoId: number, cursoId: number): string {
+  getEstadoInscripcion(alumnoId: number, cursoId: number): string {
     // Buscamos la inscripción que corresponda al alumno y curso
     const inscripcion = this.inscripcionService.getInscripciones()
       .find(i => i.alumnoId === alumnoId && i.cursoId === cursoId);
-
     return inscripcion ? inscripcion.estado : 'Sin inscripción';
   }
 }
